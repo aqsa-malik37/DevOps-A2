68bb676e01d1817ec06f315d17cf7bdfceb230a9
Laraib Ahmad <148629756+LaraibAhmad78@users.noreply.github.com>
2023-10-31 19:55:53 +0500

    Update webscrap.py

--- webscrap.py
+++ webscrap.py
@@ -7,5 +7,10 @@ response = requests.get("https://scholar.google.com")
 html = BeautifulSoup(response.text, "html.parser")
 
 print(html.prettify())
-    
+
+   import random
+
+random_number = random.randint(1, 10)
+print(f"The random number is: {random_number}")
+
 
